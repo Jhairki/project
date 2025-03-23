86eb701a497e0fc843da733d50eb6f8833bef2d3
proyect <jrlauramercado@gmail.com>
2025-03-23 01:57:00 -0400

    first17

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 
 
-fetch('../../navbar.html')
+fetch('navbar.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('navbar-container').innerHTML = data;
     })
     .catch(error => console.error('Error al cargar el navbar:', error));
 
-fetch('../../footer.html')
+fetch('footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-container').innerHTML = data;
